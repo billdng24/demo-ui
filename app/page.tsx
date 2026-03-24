42b5ef3263e4847fd2754b10217dbc698286b894
nguyendv-amira <nguyen_dv@amira.vn>
2026-03-24 16:04:12 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
   return (
     <div>
-      <h1>Đàm Văn Nguyên xyzzzz</h1>
+      <h1>Đàm Văn Nguyên xyzzzzcc</h1>
       <h1>NextJS CI/CD Demo</h1>
       <pre>{JSON.stringify(data)}</pre>
     </div>
